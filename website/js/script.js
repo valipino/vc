eb616caeb89178a4be139feb507f51fe8cc06a7c
valipino <vali.pino@gmx.de>
2023-11-18 19:20:34 +0100

    Side Bar added

--- website/js/script.js
+++ website/js/script.js
@@ -19,10 +19,25 @@ function updateVisibility() {
 }
 
 const body = document.body;
-  
-function toggleDarkMode() {
-    body.classList.toggle('light-mode');
-}
 
+function toggleDarkMode() {
+  body.classList.toggle('light-mode');
+}
 // Show the first card initially
 updateVisibility();
+
+
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+  menuBtnChange();
+});
+
+function menuBtnChange(){
+  if(sidebar.classList.contains("open")){
+    closeBtn.classList.replace("fa-solid fa-bars", "fa-solid fa-bars-staggered");
+  } else {
+    closeBtn.classList.replace("fa-solid fa-bars-staggered", "fa-solid fa-bars");
+  }
+}
